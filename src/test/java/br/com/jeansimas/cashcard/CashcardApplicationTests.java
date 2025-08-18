dbcb62d271ad2bace3026cf5334e532e1b1929b2
J3ANSIMAS <jeansimas16@gmail.com>
2025-08-18 11:17:24 -0300

    Remove unused GraphQlProperties import from test class

--- src/test/java/br/com/jeansimas/cashcard/CashcardApplicationTests.java
+++ src/test/java/br/com/jeansimas/cashcard/CashcardApplicationTests.java
@@ -4,7 +4,6 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
