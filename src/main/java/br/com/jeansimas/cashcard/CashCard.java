34f84fbd74ec7fa17edc133645f32072986e791e
J3ANSIMAS <jeansimas16@gmail.com>
2025-08-19 18:00:12 -0300

    Integrate Spring Data JDBC for CashCard entity storage and update Controller to use repository

--- src/main/java/br/com/jeansimas/cashcard/CashCard.java
+++ src/main/java/br/com/jeansimas/cashcard/CashCard.java
@@ -1,4 +1,6 @@
 package br.com.jeansimas.cashcard;
 
-record CashCard(Long id, Double amount) {
+import org.springframework.data.annotation.Id;
+record CashCard(@Id Long id, Double amount) {
+
 }
---
+++ src/main/java/br/com/jeansimas/cashcard/CashCardRepository.java
@@ -0,0 +1,5 @@
+package br.com.jeansimas.cashcard;
+
+import  org.springframework.data.repository.CrudRepository;
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
