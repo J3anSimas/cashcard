09eb5247c2998ef5feef2d53f52cb4e2eca6ff73
J3ANSIMAS <jeansimas16@gmail.com>
2025-08-25 16:57:25 -0300

    Add get all cashcards implementation

--- src/main/java/br/com/jeansimas/cashcard/CashCardRepository.java
+++ src/main/java/br/com/jeansimas/cashcard/CashCardRepository.java
@@ -1,5 +1,7 @@
 package br.com.jeansimas.cashcard;
 
 import  org.springframework.data.repository.CrudRepository;
-interface CashCardRepository extends CrudRepository<CashCard, Long> {
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
