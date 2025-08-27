844101e357596e552410a194113447666572631c
J3ANSIMAS <jeansimas16@gmail.com>
2025-08-27 17:13:08 -0300

    Add security features and update CashCard model
    - Introduced Spring Security for authentication and authorization.
    - Updated CashCard model to include 'owner' field.
    - Modified SQL schema to reflect changes in CashCard structure.
    - Updated tests to accommodate new security measures and CashCard structure.

--- src/main/java/br/com/jeansimas/cashcard/CashCard.java
+++ src/main/java/br/com/jeansimas/cashcard/CashCard.java
@@ -1,6 +1,7 @@
 package br.com.jeansimas.cashcard;
 
 import org.springframework.data.annotation.Id;
-record CashCard(@Id Long id, Double amount) {
+
+record CashCard(@Id Long id, Double amount, String owner) {
 
 }
--- src/test/java/br/com/jeansimas/cashcard/CashCardJsonTest.java
+++ src/test/java/br/com/jeansimas/cashcard/CashCardJsonTest.java
@@ -1,4 +1,5 @@
 package br.com.jeansimas.cashcard;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +20,18 @@ class CashCardJsonTest {
     private JacksonTester<CashCard[]> jsonList;
 
     private CashCard[] cashCards;
+
     @BeforeEach
     void setup() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.0),
-                new CashCard(101L, 150.0)        );
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.0, "sarah1"),
+                new CashCard(101L, 150.0, "sarah1"));
     }
+
     @Test
     void cashCardSerializationTest() throws IOException {
-        CashCard cashCard = new CashCard(99L, 123.45);
+        CashCard cashCard = new CashCard(99L, 123.45, "sarah1");
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
@@ -41,36 +44,42 @@ class CashCardJsonTest {
         String expected = """
                 {
                   "id": 99,
-                  "amount": 123.45
+                  "amount": 123.45,
+                  "owner": "sarah1"
                 }
                 """;
-        assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45, "sarah1"));
         assertThat(json.parseObject(expected).id()).isEqualTo(99);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
+
     @Test
     void cashCardListSerializationTest() throws IOException {
         assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
 
     }
+
     @Test
     void cashCardListDeserializationTest() throws IOException {
         String expected = """
-                [
-                  {
-                    "id": 99,
-                    "amount": 123.45
-                  },
-                  {
-                    "id": 100,
-                    "amount": 1.0
-                  },
-                  {
-                    "id": 101,
-                    "amount": 150.0
-                  }
-                ]
-        """;
+                        [
+                          {
+                            "id": 99,
+                            "amount": 123.45,
+                            "owner": "sarah1"
+                          },
+                          {
+                            "id": 100,
+                            "amount": 1.0,
+                            "owner": "sarah1"
+                          },
+                          {
+                            "id": 101,
+                            "amount": 150.0,
+                            "owner": "sarah1"
+                          }
+                        ]
+                """;
         assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
     }
 }
